e880a0eb411dd3b1c2d96d2c8b557f83d3ef5ce7
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 23:28:43 +0300

    Заглушка контента

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <main className="">
-      Hello world
+    <main>
+      <div className="flex flex-col justify-center items-center">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
     </main>
   )
 }
