398c14763fa7ffddd6f6f73db20c670a7e3c2f6e
ZNP8b <znp8b00ff@gmail.com>
2023-10-26 02:07:42 +0300

    Уменьшил слеш

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="flex flex-col text-2xl justify-center items-center">
       <span >Пироги номер один</span>
       <Clock />
-      <span >18'321'303 / <span className="text-base">105.05%</span></span>
+      <span >18'321'303<span className="text-base"> / 105.05%</span></span>
     </div>
   )
 }
