d5338b9123fa3dfdb574f211816059a7a94e5ec9
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 23:47:17 +0300

    Немного верстки карточек

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col justify-center items-center">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+      <div className="flex flex-col justify-center items-center gap-3 p-3">
+        <div className="cardItem" >1</div>
+        <div className="cardItem" >2</div>
+        <div className="cardItem" >3</div>
       </div>
     </main>
   )
