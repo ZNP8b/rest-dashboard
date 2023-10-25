d40431199d960588f89a52c601968ae2c93aa698
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 23:59:50 +0300

    Сменил шрифт на поппинс

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Roboto } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 
-const roboto = Roboto({ weight: ['100', '300', '400', '700'], subsets: ['latin'] })
+const poppins = Poppins({ weight: ['100', '300', '400', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Restaurant dashboard',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className + ' h-screen'}>
+      <body className={poppins.className + ' h-screen'}>
         <div className='bg-gray-100 min-h-full'>
           <Header />
           {children}
