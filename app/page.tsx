5cee976c9c15ffa38f50dcdcfc9d39c455e29d1a
ZNP8b <znp8b00ff@gmail.com>
2023-10-26 02:06:08 +0300

    Тестовая карточка

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,16 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-col justify-center items-center gap-3 p-3">
-        <div className="cardItem" >1</div>
-        <div className="cardItem" >2</div>
-        <div className="cardItem" >3</div>
+        <div className="cardItem" >
+          <div className="border-b-2 border-gray-400/60">
+            <h1 className="text-xl bg-sky-200">Выручка</h1>
+          </div>
+          <div>
+            <p className="text-4xl text-blue-700 pt-3">198'591<span className="text-2xl"> / 76</span></p>
+            <p className="text-lg">231 400<span className="text-sm"> / 89 ↘</span></p>
+          </div>
+        </div>
+
       </div>
     </main>
   )
