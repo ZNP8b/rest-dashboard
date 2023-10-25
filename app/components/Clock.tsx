6845823213bf83379f47f0e684ef85d8a7eb5206
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 22:57:15 +0300

    Массив названий дней недели

--- app/components/Clock.tsx
+++ app/components/Clock.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 
 const Clock = () => {
   const [time, setTime] = useState(new Date())
+  const daysOfTheWeek = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ']
 
   return (
     <div>Clock</div>
