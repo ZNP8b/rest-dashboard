e00d8d5dc510f1de75424464deb29de52ed41247
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 23:29:04 +0300

    Мобильная верстка Header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,7 @@ import Clock from "./Clock"
 const Header = () => {
 
   return (
-    <div className="flex justify-around">
+    <div className="flex flex-col text-2xl justify-center items-center bg-gray-300">
       <span>Название ресторана</span>
       <Clock />
       <span>Выручка за месяц</span>
