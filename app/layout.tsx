d999cb271c4974ad88a4a9370b45eab4050c3f71
ZNP8b <znp8b00ff@gmail.com>
2023-10-25 23:28:25 +0300

    Сменил шрифт на Roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ weight: ['100', '300', '400', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Restaurant dashboard',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <div className='bg-gray-100 h-[100dvh]'>
           <Header />
           {children}
