cd13337dfc6ef1593733c8ed5bca04399595ed94
ZNP8b <znp8b00ff@gmail.com>
2023-10-29 02:50:07 +0300

    Немного фикса адаптива

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,10 +3,11 @@ import Clock from "./Clock"
 const Header = () => {
 
   return (
-    <div className="flex flex-col text-2xl justify-center items-center">
+    <div className="flex flex-col py-5 text-2xl justify-center items-center
+    lg:flex-row lg:justify-between xl:px-20 lg:px-10 xl:text-4xl lg:text-3xl">
       <span >Пироги номер один</span>
       <Clock />
-      <span >18'321'303<span className="text-base"> / 105.05%</span></span>
+      <span >18'321'303<span className="text-base xl:text-2xl"> / 105.05%</span></span>
     </div>
   )
 }
