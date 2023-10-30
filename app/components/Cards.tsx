e777e3d562103717af260db572f7a72776aa3595
ZNP8b <znp8b00ff@gmail.com>
2023-10-31 00:25:20 +0300

    Заготовки карточек

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -1,7 +1,17 @@
 
 const Cards = () => {
   return (
-    <div></div>
+
+    <div className="CardWrapper">
+      <div className="CardItem-1">1</div>
+      <div className="CardItem-2">2</div>
+      <div className="CardItem-3">3</div>
+      <div className="CardItem-4">4</div>
+      <div className="CardItem-5">5</div>
+      <div className="CardItem-6">6</div>
+      <div className="CardItem-7">7</div>
+    </div>
+
   )
 }
 
