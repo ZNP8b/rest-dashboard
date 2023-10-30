a8e16756df6878dfba9510391fb4001144b06649
ZNP8b <znp8b00ff@gmail.com>
2023-10-31 01:09:47 +0300

    Рендеринг карточки через map

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -1,15 +1,67 @@
+import Card from "./Card"
+
+
+const data = [
+  {
+    title: 'Среднее время приготовления',
+    todayAmount: '10000',
+    prevAmount: '10000',
+    networkAverage: '10000'
+  },
+  {
+    title: 'Среднее время на полке',
+    todayAmount: '20000',
+    prevAmount: '20000',
+    networkAverage: '20000'
+  },
+  {
+    title: 'Среднее время доставки',
+    todayAmount: '30000',
+    prevAmount: '30000',
+    networkAverage: '30000'
+  },
+  {
+    title: 'Выручка',
+    todayAmount: '',
+    prevAmount: '',
+    networkAverage: ''
+  },
+  {
+    title: 'Средний чек',
+    todayAmount: '',
+    prevAmount: '',
+    networkAverage: ''
+  },
+
+
+]
+
 
 const Cards = () => {
   return (
 
     <div className="CardWrapper">
-      <div className="CardItem-1">1</div>
-      <div className="CardItem-2">2</div>
-      <div className="CardItem-3">3</div>
-      <div className="CardItem-4">4</div>
-      <div className="CardItem-5">5</div>
-      <div className="CardItem-6">6</div>
-      <div className="CardItem-7">7</div>
+
+      {/* Карточка */}
+
+      {
+        data.map((item, index) => (
+          <Card
+            index={index}
+            title={item.title}
+            todayAmount={item.todayAmount}
+            prevAmount={item.prevAmount}
+            networkAverage={item.networkAverage} />
+        ))
+      }
+
+      <div className="CardItem-6">
+        <h1 className="CardHeader">Неделя</h1>
+      </div>
+
+      <div className="CardItem-7">
+        <h1 className="CardHeader">Предзаказы</h1>
+      </div>
     </div>
 
   )
