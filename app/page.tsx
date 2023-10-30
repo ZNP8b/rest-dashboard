0943057e41c6c8c86eceb680ee966583f1dcd3ae
ZNP8b <znp8b00ff@gmail.com>
2023-10-30 20:33:05 +0300

    Разные тесты карточек

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,86 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center gap-3 p-3
       lg:grid xl:grid-cols-4 xl:grid-rows-3 h-[calc(100vh-80px)]
       lg:grid-cols-3 lg:grid-rows-3">
+
         <div className="cardItem" >
-          <h1>Среднее время приготовления</h1>
+          <h1 className="bg-orange-300 p-3" >Среднее время приготовления</h1>
+
+          <div className="flex flex-col gap-4 justify-center items-center h-full">
+
+            <div className="flex flex-col gap-2">
+              <h1 className="text-blue-700 text-base">Сегодня</h1>
+              <p className="text-blue-700 text-5xl">100000</p>
+            </div>
+
+            <div className="flex justify-center items-center gap-4">
+              <div className="flex flex-col gap-2">
+                <h1 className="text-sm">Предыдущий период</h1>
+                <p className="text-4xl">100000</p>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h1 className="text-sm">Серднее по сети</h1>
+                <p className="text-4xl">100000</p>
+              </div>
+            </div>
+
+          </div>
+
         </div>
+
+
         <div className="cardItem col-start-1 row-start-2">
-          <h1>Среднее время на полке</h1>
+          <h1 className="bg-orange-200 p-3" >Среднее время на полке</h1>
+
+          <div className="flex gap-4 items-center justify-center mt-3 h-full">
+            <div>
+              <h1 className="text-blue-700">Сегодня</h1>
+              <p className="text-blue-700">123</p>
+            </div>
+            <div>
+              <h1>Предыдущий период</h1>
+              <p>123</p>
+            </div>
+            <div>
+              <h1>Серднее по сети</h1>
+              <p>123</p>
+            </div>
+          </div>
+
         </div>
+
         <div className="cardItem col-start-1 row-start-3">
-          <h1>Среднее время доставки</h1>
+          <h1 className="bg-orange-200 p-3">Среднее время доставки</h1>
+
+          <div className="flex gap-4 items-center justify-center mt-3 h-full">
+            <div>
+              <h1 className="text-blue-700">Сегодня</h1>
+              <p className="text-blue-700">123</p>
+            </div>
+            <div>
+              <h1>Предыдущий период</h1>
+              <p>123</p>
+            </div>
+            <div>
+              <h1>Серднее по сети</h1>
+              <p>123</p>
+            </div>
+          </div>
+
         </div>
         <div className="cardItem col-start-2 row-start-1">
-          <h1>Выручка</h1>
+          <h1 className="bg-blue-300 p-3">Выручка</h1>
         </div>
         <div className="cardItem xl:col-start-3 xl:row-start-1
         lg:col-start-2 lg:row-start-2">
-          <h1>Средний чек</h1>
+          <h1 className="bg-blue-300 p-3">Средний чек</h1>
         </div>
         <div className="cardItem xl:col-span-2 xl:row-span-2 col-start-2 xl:row-start-2
         lg:row-start-3">
-          <h1>Неделя</h1>
+          <h1 className="bg-blue-300 p-3">Неделя</h1>
         </div>
         <div className="cardItem row-span-3 xl:col-start-4 row-start-1
         lg:col-start-3">
-          <h1>Заказы</h1>
+          <h1 className="bg-green-300 p-3">Заказы</h1>
         </div>
       </div>
 
