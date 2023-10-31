13da172e4ddf713763e2dd860afb2dbf10b485b3
ZNP8b <znp8b00ff@gmail.com>
2023-11-01 01:49:09 +0300

    Добавить саму таблицу с двумя записями для примера

--- app/components/WeekTable.tsx
+++ app/components/WeekTable.tsx
@@ -1,7 +1,51 @@
 
 const WeekTable = () => {
   return (
-    <div>WeekTable</div>
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="w-full text-center text-sm text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr >
+            <th scope="col" className="px-6 py-3">
+              Дата
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Заказы
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Выручка
+            </th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              17-09-2023
+            </th>
+            <td className="px-6 py-4">
+              100
+            </td>
+            <td className="px-6 py-4">
+              242 322
+            </td>
+          </tr>
+
+          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              18-09-2023
+            </th>
+            <td className="px-6 py-4">
+              232
+            </td>
+            <td className="px-6 py-4">
+              366 687
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
   )
 }
 
