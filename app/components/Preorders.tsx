742936c7c3f78821974b26cb146975f1dc0a09ad
ZNP8b <znp8b00ff@gmail.com>
2023-11-01 04:36:13 +0300

    Тестовый preorders

--- app/components/Preorders.tsx
+++ app/components/Preorders.tsx
@@ -1,7 +1,17 @@
+const data = ['1', '2', '3', '4']
 
 const Preorders = () => {
+
   return (
-    <div>Preorders</div>
+    <div className="flex flex-col-reverse">
+      {
+        data.map((item, index) => (
+          <div key={index} className={`even:bg-gray-100 odd:bg-white p-2 text-center`}>
+            {item}
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
