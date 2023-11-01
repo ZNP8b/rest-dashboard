4fbe35183bb063edee338b044eb4db9909b60dfd
ZNP8b <znp8b00ff@gmail.com>
2023-11-01 03:20:02 +0300

    Заполнение таблицы с помощью map

--- app/components/WeekTable.tsx
+++ app/components/WeekTable.tsx
@@ -1,3 +1,56 @@
+'use client'
+
+import moment from "moment"
+import 'moment/locale/ru'
+moment.locale('ru')
+
+const getWeekFromToday = () => {
+  let dateArray = []
+  for (let index = 0; index < 7; index++) {
+    dateArray.push(moment().add(index, 'days').format('L'))
+  }
+  return dateArray
+}
+
+const todayWeek = getWeekFromToday()
+
+const data = [
+  {
+    date: todayWeek[0],
+    orders: '100',
+    revenue: '242 322'
+  },
+  {
+    date: todayWeek[1],
+    orders: '100',
+    revenue: '242 322'
+  },
+  {
+    date: todayWeek[2],
+    orders: '100',
+    revenue: '242 322'
+  },
+  {
+    date: todayWeek[3],
+    orders: '100',
+    revenue: '242 322'
+  },
+  {
+    date: todayWeek[4],
+    orders: '100',
+    revenue: '242 322'
+  },
+  {
+    date: todayWeek[5],
+    orders: '100',
+    revenue: '242 322'
+  },
+  {
+    date: todayWeek[6],
+    orders: '100',
+    revenue: '242 322'
+  },
+]
 
 const WeekTable = () => {
   return (
@@ -5,41 +58,68 @@ const WeekTable = () => {
       <table className="w-full text-center text-sm text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr >
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-5 py-2">
               Дата
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-5 py-2">
               Заказы
             </th>
-            <th scope="col" className="px-6 py-3">
+            <th scope="col" className="px-5 py-2">
               Выручка
             </th>
           </tr>
         </thead>
 
         <tbody>
+          {
+            data.map((item) => (
+              <tr className="even:bg-gray-50 odd:bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {item.date}
+                </th>
+                <td className="px-4 py-3">
+                  {item.orders}
+                </td>
+                <td className="px-4 py-3">
+                  {item.revenue}
+                </td>
+              </tr>
+            ))
+          }
 
-          <tr className="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              17-09-2023
+          <tr className="even:bg-gray-50 odd:bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              СР.НЕДЕЛЯ
             </th>
-            <td className="px-6 py-4">
-              100
+            <td className="px-4 py-3">
+              181
             </td>
-            <td className="px-6 py-4">
-              242 322
+            <td className="px-4 py-3">
+              235 356
             </td>
           </tr>
 
-          <tr className="border-b bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              18-09-2023
+          <tr className="even:bg-gray-50 odd:bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              ПРЕД.НЕДЕЛЯ
             </th>
-            <td className="px-6 py-4">
-              232
+            <td className="px-4 py-3">
+              181
             </td>
-            <td className="px-6 py-4">
-              366 687
+            <td className="px-4 py-3">
+              235 356
+            </td>
+          </tr>
+
+          <tr className="even:bg-gray-50 odd:bg-white dark:bg-gray-900 dark:border-gray-700">
+            <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              СР.МЕСЯЦ
+            </th>
+            <td className="px-4 py-3">
+              181
+            </td>
+            <td className="px-4 py-3">
+              235 356
             </td>
           </tr>
 
